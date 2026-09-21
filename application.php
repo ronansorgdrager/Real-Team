20e0d5930289f78230a7ebf57dc99cb34faed7c4
rd-graham <rhysgraham799@gmail.com>
2026-09-21 15:45:21 +1000

    hello

--- application.php
+++ application.php
@@ -76,7 +76,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'search') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wireframe</title>
+    <title>Cricket Statistics Explorer</title>
     <style>
         body {
             margin: 0;
